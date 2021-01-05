4f097ffb61497a2a43b1f652a339830e28916cdc
brianpinto91 <brianpinto91@gmail.com>
2021-01-05 23:10:25 +0100

    implement pixel-coord normalization

--- utils.py
+++ utils.py
@@ -35,3 +35,38 @@ def get_matches(img_left_gray, img_right_gray, num_keypoints=1000, threshold=0.8
         good_kp_l.append(kp_l[match.queryIdx].pt) # keypoint on the left image
         good_kp_r.append(kp_r[match.trainIdx].pt) # matching keypoint on the right image
     return np.array(good_kp_l), np.array(good_kp_r)
+
+def normalize_pixels_coordinates(points_img_a, points_img_b):
+    '''Function to normalize the pixel co-ordinates to have zero mean and unit standard deviation.
+
+    Args:
+        points_img_a (numpy array): of shape (n, 2) representing the points of Image A
+        points_img_b (numpy array): of shape (n, 2) representing the matching points of Image B
+    
+    Returns:
+        norm_points_img_a (numpy array): of shape (n, 2)
+        norm_points_img_b (numpy array): of shape (n, 2)
+        T_mat (numpy array): Transformation matrix that was used to normalize points
+    '''
+    num_points = points_img_a.shape[0]
+    x_all_points = np.concatenate((points_img_a[:, 0], points_img_b[:, 0]), axis=0)
+    y_all_points = np.concatenate((points_img_a[:, 1], points_img_b[:, 1]), axis=0) 
+    mu_x =  np.mean(x_all_points)
+    std_x = np.std(x_all_points)
+    mu_y = np.mean(y_all_points)
+    std_y = np.std(y_all_points)
+    # normalization using x_n = (x - mu)/std
+    # implemented as a matrix to take advantage of matrix multiplication to normalize
+    epsilon = 1e7 # for numerical stability while division
+    T_mat = np.array([[1 / (std_x + epsilon), 0, -mu_x / (std_x + epsilon)],
+                      [0, 1 / (std_y + epsilon), -mu_y / (std_y + epsilon)],
+                      [0, 0, 1]])
+    
+    # add a 3rd column of ones to the point numpy representation to make use of matrix multiplicate transform
+    ones_col = np.ones((num_points,1))
+    points_img_a = np.concatenate((points_img_a, ones_col), axis=1)
+    points_img_b = np.concatenate((points_img_b, ones_col), axis=1)
+
+    norm_points_img_a = (np.matmul(T_mat, points_img_a.T)).T[:,0:2]
+    norm_points_img_b = (np.matmul(T_mat, points_img_b.T)).T[:,0:2]
+    return norm_points_img_a, norm_points_img_b, T_mat
