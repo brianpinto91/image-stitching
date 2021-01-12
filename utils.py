0d187c383236cddbe07c6f53c03621bffffeff96
brianpinto91 <brianpinto91@gmail.com>
2021-01-12 18:23:40 +0100

    rename variables

--- utils.py
+++ utils.py
@@ -1,26 +1,26 @@
 import cv2
 import numpy as np
 
-def get_matches(img_left_gray, img_right_gray, num_keypoints=1000, threshold=0.8):
+def get_matches(img_a_gray, img_b_gray, num_keypoints=1000, threshold=0.8):
     '''Function to get matched keypoints from two images using ORB
 
     Args:
-        img_left_gray (numpy array): of shape (H, W, C) with opencv representation of left image (i.e C: B,G,R)
-        img_right_gray (numpy array): of shape (H, W, C) with opencv representation of right image (i.e C: B,G,R)
+        img_a_gray (numpy array): of shape (H, W, C) with opencv representation of image A (i.e C: B,G,R)
+        img_b_gray (numpy array): of shape (H, W, C) with opencv representation of image B (i.e C: B,G,R)
         num_keypoints (int): number of points to be matched (default=100)
         threshold (float): can be used to filter strong matches only. Lower the value, stronger the requirements and hence fewer matches.
     Returns:
-        match_points_a (numpy array): of shape (n, 2) representing x,y pixel coordinates of left image keypoints
-        match_points_b (numpy array): of shape (n, 2) representing x,y pixel coordianted of matched keypoints in right image
+        match_points_a (numpy array): of shape (n, 2) representing x,y pixel coordinates of image A keypoints
+        match_points_b (numpy array): of shape (n, 2) representing x,y pixel coordianted of matched keypoints in image B
     '''
     orb = cv2.ORB_create(nfeatures=num_keypoints)
-    kp_l, desc_l = orb.detectAndCompute(img_left_gray, None)
-    kp_r, desc_r = orb.detectAndCompute(img_right_gray, None)
+    kp_a, desc_a = orb.detectAndCompute(img_a_gray, None)
+    kp_b, desc_b = orb.detectAndCompute(img_b_gray, None)
     
     dis_matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
-    matches_list = dis_matcher.knnMatch(desc_l, desc_r, k=2) # get the two nearest matches for each keypoint in left image
+    matches_list = dis_matcher.knnMatch(desc_a, desc_b, k=2) # get the two nearest matches for each keypoint in image A
 
-    # for each keypoint feature in left image, compare the distance of the two matched keypoints in the right image
+    # for each keypoint feature in image A, compare the distance of the two matched keypoints in image B
     # retain only if distance is less than a threshold 
     good_matches_list = []
     for match_1, match_2 in matches_list:
@@ -28,22 +28,23 @@ def get_matches(img_left_gray, img_right_gray, num_keypoints=1000, threshold=0.8
             good_matches_list.append(match_1)
     
     #filter good matching keypoints 
-    good_kp_l = []
-    good_kp_r = []
+    good_kp_a = []
+    good_kp_b = []
 
     for match in good_matches_list:
-        good_kp_l.append(kp_l[match.queryIdx].pt) # keypoint on the left image
-        good_kp_r.append(kp_r[match.trainIdx].pt) # matching keypoint on the right image
-    return np.array(good_kp_l), np.array(good_kp_r)
+        good_kp_a.append(kp_a[match.queryIdx].pt) # keypoint in image A
+        good_kp_b.append(kp_b[match.trainIdx].pt) # matching keypoint in image B
+    return np.array(good_kp_a), np.array(good_kp_b)
 
 
-def calculate_homography(points_img_l, points_img_r):
+def calculate_homography(points_img_a, points_img_b):
     '''Function to calculate the homography matrix from point corresspondences using Direct Linear Transformation
+        The resultant homography transforms points in image B into points in image A
         Homography H = [h1 h2 h3; 
                         h4 h5 h6;
                         h7 h8 h9]
-        u, v ---> point in the left image
-        x, y ---> matched point in the right image then,
+        u, v ---> point in image A
+        x, y ---> matched point in image B then,
         with n point correspondences the DLT equation is:
             A.h = 0
         where A = [-x1 -y1 -1 0 0 0 u1*x1 u1*y1 u1;
@@ -55,18 +56,18 @@ def calculate_homography(points_img_l, points_img_r):
         This equation is then solved using SVD
         (At least 4 point correspondences are required to determine 8 unkwown parameters of homography matrix)
     Args:
-        points_img_l (numpy array): of shape (n, 2) representing pixel coordinate points (u, v) in the left image
-        points_img_r (numpy array): of shape (n, 2) representing pixel coordinates (x, y) in the right image
+        points_img_a (numpy array): of shape (n, 2) representing pixel coordinate points (u, v) in image A
+        points_img_b (numpy array): of shape (n, 2) representing pixel coordinates (x, y) in image B
     
     Returns:
         h_mat: A (3, 3) numpy array of estimated homography
     '''
     # concatenate the two numpy points array to get 4 columns (u, v, x, y)
-    points_lr = np.concatenate((points_img_l, points_img_r), axis=1)
+    points_a_and_b = np.concatenate((points_img_a, points_img_b), axis=1)
     A = []
-    # fill the A matrix by looping through each row of points_ab containing u, v, x, y
+    # fill the A matrix by looping through each row of points_a_and_b containing u, v, x, y
     # each row in the points_ab would fill two rows in the A matrix
-    for u, v, x, y in points_lr:
+    for u, v, x, y in points_a_and_b:
         A.append([-x, -y, -1, 0, 0, 0, u*x, u*y, u])
         A.append([0, 0, 0, -x, -y, -1, v*x, v*y, v])
     
@@ -78,35 +79,35 @@ def calculate_homography(points_img_l, points_img_r):
     return h_mat
 
 
-def compute_outliers(h_mat, points_img_l, points_img_r, threshold=3):
+def compute_outliers(h_mat, points_img_a, points_img_b, threshold=3):
     '''Function to compute the error in the Homography matrix using the matching points in
         image A and image B
     
     Args:
-        h_mat (numpy array): of shape (3, 3) representing the homography that transforms points in right image to the left image
-        points_img_l (numpy array): of shape (n, 2) representing pixel coordinate points (u, v) in the left image
-        points_img_r (numpy array): of shape (n, 2) representing pixel coordinates (x, y) in the right image
+        h_mat (numpy array): of shape (3, 3) representing the homography that transforms points in image B to points in image A
+        points_img_a (numpy array): of shape (n, 2) representing pixel coordinate points (u, v) in image A
+        points_img_b (numpy array): of shape (n, 2) representing pixel coordinates (x, y) in image B
         theshold (int): a number that represents the allowable euclidean distance (in pixels) between the transformed pixel coordinate from
-            the right image to the supposed to be pixel coordinate in the left image, to be conisdered outliers
+            the image B to the matched pixel coordinate in image A, to be conisdered outliers
     
     Returns:
         error: a scalar float representing the error in the Homography matrix
     '''
-    num_points = points_img_l.shape[0]
+    num_points = points_img_a.shape[0]
     outliers_count = 0
 
     # add a 3rd column of ones to the point numpy representation to make use of matrix multiplication
-    ones_col = np.ones((num_points,1))
-    points_img_l = np.concatenate((points_img_l, ones_col), axis=1)
-    points_img_r = np.concatenate((points_img_r, ones_col), axis=1)
-    points_img_r_hat = np.matmul(h_mat, points_img_r.T).T
-    points_img_r_hat = points_img_r_hat / (points_img_r_hat[:,2]).reshape(-1,1)
-    # let x, y be coordinate representation of points in the left image
-    # let x_hat, y_hat be the coordinate representation of transformed points of the right image with resect to left image
-    x = points_img_l[:, 0]
-    y = points_img_l[:, 1]
-    x_hat = points_img_r_hat[:, 0]
-    y_hat = points_img_r_hat[:, 1]
+    ones_col = np.ones((num_points, 1))
+    points_img_a = np.concatenate((points_img_a, ones_col), axis=1)
+    points_img_b = np.concatenate((points_img_b, ones_col), axis=1)
+    points_img_b_hat = np.matmul(h_mat, points_img_b.T).T
+    points_img_b_hat = points_img_b_hat / (points_img_b_hat[:,2]).reshape(-1,1)
+    # let x, y be coordinate representation of points in image A
+    # let x_hat, y_hat be the coordinate representation of transformed points of image B with respect to image A
+    x = points_img_a[:, 0]
+    y = points_img_a[:, 1]
+    x_hat = points_img_b_hat[:, 0]
+    y_hat = points_img_b_hat[:, 1]
     euclid_dis = np.sqrt(np.power((x_hat - x), 2) + np.power((y_hat - y), 2)).reshape(-1)
     for dis in euclid_dis:
         if dis > threshold:
@@ -114,21 +115,21 @@ def compute_outliers(h_mat, points_img_l, points_img_r, threshold=3):
     return outliers_count
 
 
-def compute_homography_ransac(matches_l, matches_r):
+def compute_homography_ransac(matches_a, matches_b):
     """Function to estimate the best homography matrix using RANSAC on potentially matching
     points.
     
     Args:
-        matches_l (numpy array): of shape (n, 2) representing the coordinates
-            of possibly matching points in the left image
-        matches_r (numpy array): of shape (n, 2) representing the coordinates
-            of possibly matching points in the right image
+        matches_a (numpy array): of shape (n, 2) representing the coordinates
+            of possibly matching points in image A
+        matches_b (numpy array): of shape (n, 2) representing the coordinates
+            of possibly matching points in image B
 
     Returns:
         best_h_mat: A numpy array of shape (3, 3) representing the best homography
-            matrix that transforms points in right image to points in left image
+            matrix that transforms points in image B to points in image A
     """
-    num_all_matches =  matches_l.shape[0]
+    num_all_matches =  matches_a.shape[0]
     # RANSAC parameters
     SAMPLE_SIZE = 5 #number of point correspondances for estimation of Homgraphy
     SUCCESS_PROB = 0.995 #required probabilty of finding H with all samples being inliners 
@@ -141,10 +142,11 @@ def compute_homography_ransac(matches_l, matches_r):
 
     for i in range(min_iterations):
         rand_ind = np.random.permutation(range(num_all_matches))[:SAMPLE_SIZE]
-        h_mat = calculate_homography(matches_l[rand_ind], matches_r[rand_ind])
-        outliers_count = compute_outliers(h_mat, matches_l, matches_r)
+        h_mat = calculate_homography(matches_a[rand_ind], matches_b[rand_ind])
+        outliers_count = compute_outliers(h_mat, matches_a, matches_b)
         if outliers_count < lowest_outliers_count:
             best_h_mat = h_mat
             lowest_outliers_count = outliers_count
             best_i = i
     return best_h_mat
+
