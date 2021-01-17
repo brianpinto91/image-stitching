114d2135114500445b6fa2487994bd9057993b1c
brianpinto91 <brianpinto91@gmail.com>
2021-01-17 18:35:51 +0100

    add project description and links

--- setup.py
+++ setup.py
@@ -1,24 +1,39 @@
-import setuptools
+""" imgstich is a python package to stich multiple images
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+It provides:
+
+- a function to join a list of images corressponding to a scene by passing image filenames in an order
+- a function to stich images along the width as well as height, but not together
+- a function to save the stiched image or return it as an numpy array
+
+For detailed information please visit the links for Homepage or Documetation on the left hand side under Project links.  
+"""
+
+
+DOCLINES = (__doc__ or '').split("\n")
+
+import setuptools
 
 setuptools.setup(
     name="imgstich",
-    version="0.0.1",
+    version="0.0.6",
     author="Brian Pinto",
     author_email="brian.pinto@tuhh.de",
-    description="A python package that can be used to stich multiple images either horizontally or vertically",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/brianpinto91/image-stiching",
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
+    url="https://brianpinto91.github.io/image-stiching",
+    project_urls={
+            "Bug Tracker": "https://github.com/brianpinto91/image-stiching/issues",
+            "Documentation": "https://brianpinto91.github.io/image-stiching",
+            "Source Code": "https://github.com/brianpinto91/image-stiching",
+        },
     packages=setuptools.find_packages(),
     install_requires=[
           'opencv-python>=4.5.1.48', 'numpy>=1.19.4',
       ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6.9',
